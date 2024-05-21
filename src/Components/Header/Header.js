e113120461842809fe465c9be61bc1b0e8a28c2d
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-22 01:56:41 +0530

    ..

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,19 +3,22 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <nav className="nav-wrapper">
-            <div className="nav-content">
-                <img className="logo" src="./assets/image/name.svg" alt="logo" />
-                <ul>
-                    <li className="menu-item">Home</li>
-                    <li className="menu-item">About</li>
-                    <li className="menu-item">Skills</li>
-                    <li className="menu-item">Work-Experience</li>
-                    <li className="menu-item">Contact Me</li>
-                </ul>
-                <button className="contact-btn" onClick={() => { }}>Hire Me</button>
-            </div>
-        </nav>
+        <>
+            <nav className="nav-wrapper">
+                <div className="nav-content">
+                    <img className="logo" src="./assets/image/name.svg" alt="logo" />
+                    <ul>
+                        <li className="menu-item">Home</li>
+                        <li className="menu-item">About</li>
+                        <li className="menu-item">Skills</li>
+                        <li className="menu-item">Work-Experience</li>
+                        <li className="menu-item">Contact Me</li>
+                    </ul>
+                    <button className="contact-btn" onClick={() => { }}>Hire Me</button>
+                </div>
+            </nav>
+        </>
+
     );
 }
 
