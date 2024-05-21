f5fbdf02afeb693d7f61000ca944db5ca9a795ce
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-21 23:56:25 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './Components/Header';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello world</h1>
+    <div>
+      <Header />
+      <HomePage />
     </div>
   );
 }
