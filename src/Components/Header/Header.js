b60720596401b89e9d9e5d2f94ee2b5e008d1ac9
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-22 01:46:57 +0530

    ..

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <>
-            <nav className="nav-wrapper">
-                <div className="nav-content">
-                    <img className="logo" src="./assets/image/logo192.png" alt="logo" />
-                    <ul>
-                        <li classNmae="menu-item">Home</li>
-
-                        <li classNmae="menu-item">About</li>
-
-                        <li classNmae="menu-item">Skills</li>
-
-                        <li classNmae="menu-item">Work-Experience</li>
-
-                        <li classNmae="menu-item">Contect Me</li>
-                        <button className="contact-btn" onClick={() => { }}>Hire Me</button>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+        <nav className="nav-wrapper">
+            <div className="nav-content">
+                <img className="logo" src="./assets/image/name.svg" alt="logo" />
+                <ul>
+                    <li className="menu-item">Home</li>
+                    <li className="menu-item">About</li>
+                    <li className="menu-item">Skills</li>
+                    <li className="menu-item">Work-Experience</li>
+                    <li className="menu-item">Contact Me</li>
+                </ul>
+                <button className="contact-btn" onClick={() => { }}>Hire Me</button>
+            </div>
+        </nav>
+    );
 }
 
-export default Header
+export default Header;
