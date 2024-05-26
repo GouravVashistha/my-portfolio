95d8a40ff724c46790197bf530dad62d236dc846
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2024-05-26 17:35:31 +0530

    hader

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <>
             <nav className="nav-wrapper">
                 <div className="nav-content">
-                    <img className="logo" src="./assets/image/name.svg" alt="logo" />
-                    
-                    <ul>
-                        <li className="menu-item">Home</li>
-                        <li className="menu-item">About</li>
-                        <li className="menu-item">Skills</li>
-                        <li className="menu-item">Work-Experience</li>
-                        <li className="menu-item">Contact Me</li>
-                    </ul>
+                    <img className="logo-class" src="./assets/image/gourav.png" alt="logo" />
+                    <div className={`menu-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+                    <div className={`menu ${menuOpen ? 'open' : ''}`}>
+                        <div className="close-icon" onClick={toggleMenu}>&times;</div>
+                        <ul>
+                            <li className="menu-item">Home</li>
+                            <li className="menu-item">About</li>
+                            <li className="menu-item">Skills</li>
+                            <li className="menu-item">Work-Experience</li>
+                            <li className="menu-item">Contact Me</li>
+                        </ul>
+                    </div>
                     <button className="contact-btn" onClick={() => { }}>Hire Me</button>
                 </div>
             </nav>
         </>
-
     );
 }
 
